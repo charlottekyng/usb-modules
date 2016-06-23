e99a64ec5f1d1c7f55c773e6b086fbdec6ca19bc
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2016-06-23 18:23:58 +0200

    increased runtime for generating genotyping VCF

--- qc/genotype.mk
+++ qc/genotype.mk
@@ -22,14 +22,14 @@ CLUSTER_VCF = $(RSCRIPT) usb-modules/qc/clusterVcf.R
 all : genotype/snps_filtered.sdp_ft.clust.png
 
 genotype/snps.vcf : $(foreach sample,$(SAMPLES),genotype/$(sample).snps.vcf)
-	$(call LSCRIPT_MEM,16G,20G,"$(LOAD_JAVA8_MODULE); $(COMBINE_VARIANTS) \
+	$(call LSCRIPT_MEM,16G,01:59:59,"$(LOAD_JAVA8_MODULE); $(COMBINE_VARIANTS) \
 		$(foreach vcf,$^,--variant $(vcf) ) -o $@ --genotypemergeoption UNSORTED -R $(REF_FASTA)")
 
 genotype/snps_filtered.vcf : genotype/snps.vcf
 	$(INIT) grep '^#' $< > $@ && grep -e '0/1' -e '1/1' $< >> $@
 
 genotype/%.snps.vcf : bam/%.bam genotype/sites.to.genotype.vcf
-	$(call LSCRIPT_PARALLEL_MEM,4,2.5G,00:59:59,"$(LOAD_JAVA8_MODULE); $(UNIFIED_GENOTYPER) \
+	$(call LSCRIPT_PARALLEL_MEM,4,2.5G,01:59:59,"$(LOAD_JAVA8_MODULE); $(UNIFIED_GENOTYPER) \
 		-nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) $(foreach bam,$(filter %.bam,$<),-I $(bam) ) \
 		--genotyping_mode GENOTYPE_GIVEN_ALLELES -alleles $(word 2,$^) -o $@ --output_mode EMIT_ALL_SITES")
 
