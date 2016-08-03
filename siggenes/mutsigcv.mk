98773cd5bd965db74a94f79316255f1a0d182525
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2016-08-03 22:42:16 +0200

    bug fix again for mutsigcv

--- siggenes/mutsigcv.mk
+++ siggenes/mutsigcv.mk
@@ -7,7 +7,7 @@ LOGDIR ?= log/mutsigcv.$(NOW)
 .DELETE_ON_ERROR:
 .PHONY : mutsigcv
 
-mutsigcv : mutsigcv/mutsigcv.siggenes.txt
-
-mutsigcv/mutsigcv.siggenes.txt : mutsigcv/mutsigcv_input.maf
-	$(call LSCRIPT_CHECK_MEM,20G,02:59:59,"$(MUTSIGCV) $(MCR) $^ $(MUTSIGCV_COVERAGE_REF) $(MUTSIGCV_COV_REF) mutsigcv $(MUTSIGCV_DICT_REF) $(MUTSIGCV_SEQ_REF_DIR)")
+mutsigcv : mutsigcv/mutsigcv.sig_genes.txt
+mutsigcv/mutsigcv.sig_genes.txt : mutsigcv/mutsigcv_input.maf
+	$(call LSCRIPT_CHECK_MEM,20G,02:59:59,"$(MUTSIGCV) $(MCR) $^ $(MUTSIGCV_COVERAGE_REF) $(MUTSIGCV_COV_REF) mutsigcv $(MUTSIGCV_DICT_REF) $(MUTSIGCV_SEQ_REF_DIR) &&
+	mv mutsigcv.* mutsigcv")
