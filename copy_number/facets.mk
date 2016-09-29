96bfcc94849412d8823bf08c2821e0f2b72c7d6f
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2016-09-29 14:54:35 +0200

    messed up the last commit

--- copy_number/facets.mk
+++ copy_number/facets.mk
@@ -1,6 +1,5 @@
 include usb-modules/Makefile.inc
 include usb-modules/config.inc
-include usb-modules/variant_callers/variantCaller.inc 
 
 LOGDIR ?= log/facets.$(NOW)
 
@@ -8,31 +7,57 @@ LOGDIR ?= log/facets.$(NOW)
 .DELETE_ON_ERROR:
 .PHONY : facets
 
-facets : $(foreach pair,$(SAMPLE_PAIRS),facets/cncf/$(pair).cncf.txt) facets/geneCN.txt
+facets : $(foreach pair,$(SAMPLE_PAIRS),facets/cncf/$(pair).cncf.txt)
+#facets/geneCN.txt
 #	facets/geneCN.txt facets/geneCN.fill.txt facets/geneCN.heatmap.pdf facets/geneCN.fill.heatmap.pdf
 
-facets/base_pos/%.gatk.vcf : vcf/%.gatk_snps.vcf
-	$(INIT) ln $< $@
+facets/vcf/dbsnp_het_gatk.snps.vcf : $(FACETS_DBSNP:.gz=) $(foreach sample,$(SAMPLES),gatk/vcf/$(sample).variants.snps.het.pass.vcf) 
+	$(call LSCRIPT_CHECK_MEM,4G,6G,"$(call GATK_MEM,3G) -T CombineVariants \
+		--minimalVCF $(foreach i,$^, --variant $i) -R $(REF_FASTA) -o $@")
 
-#FACETS_DBSNP = $(if $(findstring 
-facets/base_pos/%.gatk.dbsnp.vcf : facets/base_pos/%.gatk.vcf $(DBSNP)
-	$(call LSCRIPT_MEM,22G,03:59:59,"$(LOAD_JAVA8_MODULE); $(call COMBINE_VARIANTS,21G) \
-	--variant $(word 1,$^) --variant $(word 2,$^) -o $@ --genotypemergeoption UNSORTED -R $(REF_FASTA)")
+# flag homozygous calls
+%.het.vcf : %.vcf
+	$(call LSCRIPT_CHECK_MEM,9G,12G,"$(call GATK_MEM,8G) -V $< -T VariantFiltration -R $(REF_FASTA) \
+		--genotypeFilterName 'hom' --genotypeFilterExpression 'isHet == 0' -o $@")
+
+%.vcf.gz : %.vcf
+	$(INIT) cat $< | gzip -c > $@
+
+%.vcf : %.vcf.gz
+	$(INIT) zcat $< > $@
+
+facets/vcf/targets_dbsnp.vcf.gz : $(TARGETS_FILE_INTERVALS)
+	$(INIT) $(BEDTOOLS) intersect -header -u -a $(DBSNP) -b $< | gzip -c > $@
+
+ifeq ($(findstring ILLUMINA,$(SEQ_PLATFORM)),ILLUMINA)
+define snp-pileup-tumor-normal
+facets/snp_pileup/$1_$2.bc.gz : bam/$1.bam bam/$2.bam
+	$$(call LSCRIPT_CHECK_MEM,3G,00:59:59,"$$(LOAD_PERL_MODULE); $$(GET_BASE_COUNTS) bam/$2.bam bam/$1.bam $$@ \
+	$$(REF_FASTA) $$(TARGETS_FILE_INTERVALS) $$(GET_BASE_COUNTS_MIN_DEPTH) $$(GET_BASE_COUNTS_MAX_DEPTH)")
+endef
+$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call snp-pileup-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+endif
+
+ifeq ($(findstring IONTORRENT,$(SEQ_PLATFORM)),IONTORRENT)
+facets/snp_pileup/%/TSVC_variants.vcf : bam/%.bam
+	$(call LSCRIPT_CHECK_MEM,20G,00:59:59,"$(TVC) -s $(DBSNP) -i $< -r $$(REF_FASTA) -o $(@D) -N 8 -t $(TVC_ROOT_DIR) --primer-trim-bed $(PRIMER_TRIM_BED)
 
 define snp-pileup-tumor-normal
-facets/snp_pileup/$2_$1.bc.gz : bam/$1.bam bam/$2.bam $$(if $$(findstring true,$$(FACETS_GATK_VARIANTS)),facets/base_pos/$1.gatk.dbsnp.vcf,$$(DBSNP))
-	$$(call LSCRIPT_CHECK_MEM,3G,00:59:59,"$$(FACETS_SNP_PILEUP) -A -d $$(FACETS_SNP_PILEUP_MAX_DEPTH) -g \
-	-q $$(FACETS_SNP_PILEUP_MINMAPQ) -Q $$(FACETS_SNP_PILEUP_MINBASEQ) -r $$(FACETS_SNP_PILEUP_MIN_DEPTH)$$(,)0 \
-	$$(word 3,$$^) $$@ $$(word 1,$$^) $$(word 2,$$^)") 
-# && $$(RM) $$(word 3,$$^)")
+facets/snp_pileup/$1_$2.bc.gz : facets/snp_pileup/$2/TSVC_variants.vcf facets/snp_pileup/$1/TSVC_variants.vcf
+	$$(call LSCRIPT_CHECK_MEM,20G,03:59:59,"$$(LOAD_SNP_EFF_MODULE); $$(LOAD_JAVA8_MODULE); $$(call COMBINE_VARIANTS,19G) \
+	$$(foreach vcf,$$^,--variant $$(vcf) ) --genotypemergeoption UNSORTED -R $$(REF_FASTA) | \
+	$$(SNP_SIFT) extractFields - CHROM POS REF ALT GEN[0].FRO GEN[0].FAO GEN[0].FXX GEN[0].DP GEN[1].FRO GEN[1].FAO GEN[1].FXX GEN[1].DP | \
+	perl -p -e \"s/$$(,)[\w]+//g;\" | perl -p -e \"s/\#CHROM.+$$$$/Chromosome$$(,)Position$$(,)Ref$$(,)Alt$$(,)File1R$$(,)File1A$$(,)File1E$$(,)File1D$$(,)File2R$$(,)File2A$$(,)File2E$$(,)File2D/g;\" | \
+	sed 's/\t/$$(,)/g;' | gzip > $$@")
 endef
-$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call snp-pileup-tumor-normal,$(normal.$(pair)),$(tumor.$(pair)))))
-#$(info NORMAL is $(normal.$(pair)))
-#$(info TUMOR is $(tumor.$(pair)))
+$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call snp-pileup-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+endif
+
+#facets/params.txt : $(foreach pair,$(SAMPLE_PAIRS),facets/snp_pileup/$(pair).bc.gz)
+	
 
 facets/cncf/%.cncf.txt : facets/snp_pileup/%.bc.gz
-	$(call LSCRIPT_CHECK_MEM,3G,00:29:59,"$(LOAD_R_MODULE); $(FACETS) --minNDepth $(FACETS_SNP_PILEUP_MIN_DEPTH) \
-	--maxNDepth $(FACETS_SNP_PILEUP_MAX_DEPTH) --snp_nbhd $(FACETS_WINDOW_SIZE) \
+	$(call LSCRIPT_CHECK_MEM,3G,00:29:59,"$(LOAD_R_MODULE); $(FACETS) \
 	--cval2 $(FACETS_CVAL2) --cval1 $(FACETS_CVAL1) --genome $(REF) --min_nhet $(FACETS_MIN_NHET) --pre_cval $(FACETS_PRE_CVAL)  \
 	--outPrefix $(@D)/$* $<")
 
@@ -47,6 +72,4 @@ facets/geneCN%heatmap.pdf  : facets/geneCN%txt
 	$(call LSCRIPT_MEM,4G,00:29:29,"$(LOAD_R_MODULE); $(FACETS_PLOT_GENE_CN) $(FACETS_PLOT_GENE_CN_OPTS) $< $@")
 
 include usb-modules/variant_callers/gatk.mk
-include usb-modules/vcf_tools/vcftools.mk
-include usb-modules/variant_callers/gatkVariantCaller.mk
 include usb-modules/bam_tools/processBam.mk
