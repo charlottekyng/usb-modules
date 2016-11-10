94cc2bf62a66c4a8867093da180f0f9ab149bd8b
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2016-11-10 17:44:56 +0100

    fixed silly typos to prevented jobs from executed

--- variant_callers/somatic/TVC.mk
+++ variant_callers/somatic/TVC.mk
@@ -4,17 +4,19 @@ include usb-modules/variant_callers/somatic/somaticVariantCaller.inc
 
 LOGDIR ?= log/tvc_somtic.$(NOW)
 
-VPATH ?= bam
-VARIANT_TYPES ?= tvc_snps tvc_indels
-PHONY += tvc_somatic
+PHONY += tvc_vcfs tvc_tables
 
-tvc_somatic : tvc_vcfs tvc_tables
+VARIANT_TYPES ?= tvc_snps tvc_indels
 tvc_vcfs : $(foreach type,$(VARIANT_TYPES),$(call SOMATIC_VCFS,$(type)) $(addsuffix .idx,$(call SOMATIC_VCFS,$(type))))
 tvc_tables : $(foreach type,$(VARIANT_TYPES),$(call SOMATIC_TABLES,$(type)))
 
+.DELETE_ON_ERROR:
+.SECONDARY:
+.PHONY : $(PHONY)
+
 define tvc-somatic-vcf
 tvc/vcf/$1_$2/TSVC_variants.vcf.gz : bam/$1.bam bam/$1.bam.bai bam/$2.bam bam/$2.bam.bai
-	$$(call LSCRIPT_PARALLEL_MEN,8,3G,00:59:59,"$$(TVC) -i $$< -n $$(word 3,$$^) -r $$(REF_FASTA) -o $$(@D) -N 8 \
+	$$(call LSCRIPT_PARALLEL_MEM,8,5G,00:59:59,"$$(TVC) -i $$< -n $$(word 3,$$^) -r $$(REF_FASTA) -o $$(@D) -N 8 \
 	$$(if $$(TARGETS_FILE_INTERVALS),-b $$(TARGETS_FILE_INTERVALS)) -p $$(TVC_SOMATIC_JSON) -m $$(TVC_MOTIF) \
 	-t $$(TVC_ROOT_DIR) --primer-trim-bed $$(PRIMER_TRIM_BED)")
 endef
@@ -34,9 +36,5 @@ vcf/%.tvc_snps.vcf : tvc/vcf/%/TSVC_variants.snps.vcf
 
 vcf/%.tvc_indels.vcf : tvc/vcf/%/TSVC_variants.indels.vcf
 	$(INIT) ln -f $< $@
-
-.DELETE_ON_ERROR:
-.SECONDARY:
-.PHONY : $(PHONY)
 
 include usb-modules/vcf_tools/vcftools.mk
