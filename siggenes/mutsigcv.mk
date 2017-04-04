96984b50449a5baa6b2ead8d887da787feb0063c
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2017-04-04 17:04:49 +0200

    fixed typo in mutsigcv

--- siggenes/mutsigcv.mk
+++ siggenes/mutsigcv.mk
@@ -9,5 +9,5 @@ LOGDIR ?= log/mutsigcv.$(NOW)
 
 mutsigcv : mutsigcv/mutsigcv.sig_genes.txt
 mutsigcv/mutsigcv.sig_genes.txt : mutsigcv/mutsigcv_input.maf
-	$(call LSCRIPT_CHECK_MEM,20G,02:59:59,"$(MUTSIGCV) $(MCR) $^ $(MUTSIGCV_COVERAGE_REF) $(MUTSIGCV_COV_REF) mutsigcv $(MUTSIGCV_DICT_REF) $(MUTSIGCV_SEQ_REF_DIR) &&
+	$(call LSCRIPT_CHECK_MEM,20G,02:59:59,"$(MUTSIGCV) $(MCR) $^ $(MUTSIGCV_COVERAGE_REF) $(MUTSIGCV_COV_REF) mutsigcv $(MUTSIGCV_DICT_REF) $(MUTSIGCV_SEQ_REF_DIR) && \
 	mv mutsigcv.* mutsigcv")
