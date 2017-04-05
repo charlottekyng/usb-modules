02689d320d6ec195193dbd0d09bb66bf97febe41
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2017-04-05 16:10:11 +0200

    allow using a custom set of samples to build pooled bam (e.g. include only female samples)

--- bam_tools/poolednormBam.mk
+++ bam_tools/poolednormBam.mk
@@ -5,7 +5,8 @@ LOGDIR ?= log/poolednorm_bam.$(NOW)
 
 poolednorm_bam : bam/poolednorm.bam $(addsuffix .bai,bam/poolednorm.bam)
 
-unprocessed_bam/poolednorm.rg.bam : $(foreach normal,$(NORMAL_SAMPLES),bam/$(normal).downsampled.bam) $(addsuffix .bai,$(foreach normal,$(NORMAL_SAMPLES),bam/$(normal).downsampled.bam))
+POOLED_NORM_SAMPLES ?= NORMAL_SAMPLES
+unprocessed_bam/poolednorm.rg.bam : $(foreach normal,$(POOLED_NORM_SAMPLES),bam/$(normal).downsampled.bam) $(addsuffix .bai,$(foreach normal,$(POOLED_NORM_SAMPLES),bam/$(normal).downsampled.bam))
 	$(MKDIR) unprocessed_bam; $(call LSCRIPT_MEM,20G,02:29:29,"$(LOAD_SAMTOOLS_MODULE); $(SAMTOOLS) merge -f $@ $(filter %.bam,$^) && $(RM) $^")
 
 include usb-modules/bam_tools/processBam.mk
