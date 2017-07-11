c6a6d20ddb684c7e3d3c9d9edfcbf2515842f856
charlottekyng <ngkiuy@login10.cluster.bc2.ch>
2017-07-11 22:26:12 +0200

    increased runtime for screen_hotspot

--- qc/screenHotspot.mk
+++ qc/screenHotspot.mk
@@ -46,7 +46,7 @@ endif
 
 ifeq ($(findstring IONTORRENT,$(SEQ_PLATFORM)),IONTORRENT)
 hotspots/%/hotspotscreen.vcf : bam/%.bam hotspots/sites.to.screen.vcf.gz hotspots/sites.to.screen.vcf.gz.tbi
-	$(call LSCRIPT_PARALLEL_MEM,8,32G,11:59:59,"$(LOAD_BCFTOOLS_MODULE); $(LOAD_JAVA8_MODULE); $(LOAD_TABIX_MODULE); \
+	$(call LSCRIPT_PARALLEL_MEM,8,32G,23:59:59,"$(LOAD_BCFTOOLS_MODULE); $(LOAD_JAVA8_MODULE); $(LOAD_TABIX_MODULE); \
 	$(TVC) -s $(word 2,$^) -i $< -r $(REF_FASTA) -o $(@D) -N 8 \
 	$(if $(TARGETS_FILE_INTERVALS),-b $(TARGETS_FILE_INTERVALS)) -p $(TVC_SENSITIVE_JSON) -m $(TVC_MOTIF) \
 	-t $(TVC_ROOT_DIR) --primer-trim-bed $(PRIMER_TRIM_BED) && \
